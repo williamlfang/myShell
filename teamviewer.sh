ddef7fbaf0526ef56b853f519c8750cf0e37ef82
“williamlfang” <william.lian.fang@gmail.com>
2018-10-20 14:50:36 +0800

    处理 teamviewer

---
+++ teamviewer.sh
@@ -0,0 +1,19 @@
+# ---------------
+# 处理 teamviewer
+# 
+# DATE: 2018-10-20
+# AUTHOR: William
+# ----------------
+
+## =============================================================================
+## 启动或停止 daemon
+sudo teamviewer --daemon stop
+
+sudo teamviewer --daemon start
+
+## 打印 id
+sudo teamviewer --info print id
+
+## 设置密码
+sudo teamviewer --passwd <password>
+## =============================================================================
